34ac6ea742099ab140578147c4f757efdc0399b7
mattgaither <benjamin.gaither@shipt.com>
2022-07-17 07:39:05 -0500

    made the comments show up correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 
 // api/notes
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '..//db/db.json'));
+  res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 router.post('/notes', (req, res) => {
@@ -16,33 +16,33 @@ router.post('/notes', (req, res) => {
     id : uuidv4()
   };
 
-  fs.readFile(path.join(__dirname, '..//db/db.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
 
-    console.log("data from '..//db/db.json'.json", data );
+    console.log("data from db.json", data );
     const parsedNotes = JSON.parse(data); 
-    console.log("parsedNOtes", parsedNotes);
+    console.log("parsedNotes", parsedNotes);
 
     console.log('newNote', newNote);
     parsedNotes.push(newNote);
 
     fs.writeFile(
-      path.join(__dirname, '..//db/db.json'),
+      path.join(__dirname, '../db/db.json'),
       JSON.stringify(parsedNotes),
       (err) => {
         if (err) throw err;
-        console.log("Note saved to '..//db/db.json'!");
+        console.log("Note saved to DB!");
       }
     );
   });
-  res.sendFile(path.join(__dirname, '..//db/db.json'));
+  res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 // :id lets us know that we are passing a dynamic parameter
 router.delete('/notes/:id', (req, res) => {
   console.log(req.params.id);
 
-  fs.readFile(path.join(__dirname, '..//db/db.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
 
     const parsedNotes = JSON.parse(data);
@@ -50,15 +50,15 @@ router.delete('/notes/:id', (req, res) => {
     const notesToKeep = parsedNotes.filter((note) => note.id !== req.params.id);
 
     fs.writeFile(
-      path.join(__dirname, '..//db/db.json'),
+      path.join(__dirname, '../db/db.json'),
       JSON.stringify(notesToKeep),
       (err) => {
         if (err) throw err;
-        console.log("Note deleted successfully from '..//db/db.json'!");
+        console.log("Note deleted successfully from DB!");
       }
     );
   });
-  res.sendFile(path.join(__dirname, '..//db/db.json'));
+  res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
-const {notes} = require('./Develop/db/db.json');
-const path = require('path');
 
-
-app.use('/static', express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
-
-app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
